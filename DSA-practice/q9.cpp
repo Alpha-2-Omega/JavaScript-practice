f22411d718f309bf6d61797ccd4b06c811b3e7ca
Niharika Shokeen <niharika.shokeen05@gmail.com>
2026-01-30 00:09:07 +0530

    Largest common prefix

--- DSA-practice/q9.cpp
+++ DSA-practice/q9.cpp
@@ -6,12 +6,14 @@
 using namespace std;
 
 string longestCommonPrefix(vector<string>& s){
+    if(s.empty()) return "";
+
     string ref = s[0];
     string ans ="";
-    for(int i=0 ; i< ref.size(); i++){
-        for(int j= 1 ; j< s.size(); j++)
-        if(ref[i] != s[j][i]){
-            break;
+    for(int i=0 ; i < ref.size(); i++){
+        for(int j = 1 ; j < s.size(); j++)
+        if(i >= s[j].size() || ref[i] != s[j][i]){
+            return ans;
         }
         else{
             ans.push_back(ref[i]);
