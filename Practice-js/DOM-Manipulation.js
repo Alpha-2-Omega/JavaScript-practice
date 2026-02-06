40c3f5b8455c86fb18174fb00647630302956708
Niharika Shokeen <niharika.shokeen05@gmail.com>
2026-02-06 21:20:28 +0530

    questions on DOM

---
+++ Practice-js/DOM-Manipulation.js
@@ -0,0 +1,11 @@
+// created a buttton
+
+let newbtn = document.createElement("button");
+newbtn.innerText = "click me";
+
+newbtn.style.backgroundColor = "red";
+newbtn.style.Color = "white";
+document.querySelector("body").prepend(newbtn);
+
+//q2
+let para = document.querySelector("p");
