31845f4126b9491801da972592b015cdf0c79e9e
Niharika Shokeen <niharika.shokeen05@gmail.com>
2026-03-01 22:35:54 +0530

    Convert array to LinkedList, traverse the LL, calculate the length of Linkedlist and search a number in LL

--- q14.cpp
+++ q14.cpp
@@ -1,4 +1,4 @@
-//Convert array to LinkedList
+//Convert array to LinkedList, traverse the LL, calculate the length of Linkedlist and search a number in LL
 
 #include<iostream>
 #include <vector>
@@ -29,8 +29,53 @@ Node* ConvertArrToLL( vector<int> &arr){
     return head;
 }
 
+ void traverseTheLL(Node* head){
+    Node* temp = head;
+    while(temp){
+        cout<< temp->data<< " ";
+        temp = temp->next;
+    }
+}
+
+int lengthOfLL(Node* head){
+    Node* temp = head;
+    int cnt = 0;
+    while(temp){       
+        temp = temp-> next;
+        cnt++;
+    }
+    return cnt;
+}
+
+bool searchAnElement(Node* head, int x){
+    Node* temp = head;
+    while(temp){
+        if(temp->data == x) {
+            return true;
+        }
+        temp = temp->next;
+    }
+    return false; 
+}
+
 int main(){
     vector<int> arr = {4, 3, 8, 12};
     Node* head = ConvertArrToLL(arr);
-    cout << head->data;
+    cout << "data at head is " << head->data<< endl;
+
+    traverseTheLL(head);
+    cout<<endl;
+
+    int length = lengthOfLL(head);
+    cout<< "length of LL is "<<length<<endl;   
+    
+    int x;
+    cout<<"enter the number you want to find: ";
+    cin>>x;
+    if(searchAnElement(head, x)){
+        cout<<" Element found!";
+    }else{
+        cout<<" Element not found!";
+    }
+    return 0;
 }
